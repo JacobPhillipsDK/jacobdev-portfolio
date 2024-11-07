289186e324e478aaf38748cf3f4870d66dde7197
Jacob Phillips <jacobphillips@outlook.dk>
2024-11-08 00:57:08 +0100

    Update next.config.mjs

--- portfolio-jacobdev/next.config.mjs
+++ portfolio-jacobdev/next.config.mjs
@@ -5,9 +5,10 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   experimental: {
     serverActions: {
-      allowedOrigins: ['localhost:3000']
-    }
-  }
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
+  output: 'standalone', // Enable standalone mode for production
 };
 
 export default withContentCollections(nextConfig);
