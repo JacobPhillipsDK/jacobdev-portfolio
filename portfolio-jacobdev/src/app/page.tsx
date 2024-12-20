ce785ea7418cb57bb56a0ab4685ca88926e105e1
Jacob Phillips <jacobphillips@outlook.dk>
2024-12-20 11:23:19 +0100

    Portfolio update

--- portfolio-jacobdev/src/app/page.tsx
+++ portfolio-jacobdev/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     return (
         <div className="relative flex min-h-[100dvh] flex-col overflow-hidden bg-background">
             <Header />
-            <main className="flex-1 relative z-10">
+                <main className="flex-1 relative z-10">
                 <Container className="space-y-4">
                     <About/>
                     <Technologies/>
